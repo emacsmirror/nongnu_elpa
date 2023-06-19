e4a2d7e663333949f552b0bec57120cc8f6e8f61
marty hiatt <martianhiatus [a t] riseup [d o t] net>
2023-06-19 19:35:05 +0200

    commentary

--- fedi.el
+++ fedi.el
@@ -27,12 +27,13 @@
 ;; <https://codeberg.org/martianh/mastodon.el> to make it easy to write
 ;; endpoint-hitting functions for JSON APIs.
 
-;; It provides `fedi-request' to generate the functions, handles constructing
-;; form data parameters and JSON payloads (for POST/PUT, etc.).
+;; It provides `fedi-request' to easily generate request functions, handles
+;; constructing form data parameters and sending JSON payloads (for POST/PUT,
+;; etc.).
 
 ;; Responses are checked with `fedi-http--triage', and processed with
 ;; `fedi-http--process-response'. If a response returns HTML, it is rendered
-;; with `shr'.
+;; with `shr', otherwise the JSON is pased and returned.
 
 ;; Because of mastodon.el works, there is also code for handling link headers
 ;; in responses. Mastodon uses these for pagination in some cases. If your
