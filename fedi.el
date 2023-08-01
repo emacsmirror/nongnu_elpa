d364400ca588f2acccbfe5930cb4f1ee3a11e1be
marty hiatt <martianhiatus [a t] riseup [d o t] net>
2023-08-01 11:09:22 +0200

    flycheck

--- fedi.el
+++ fedi.el
@@ -203,16 +203,13 @@ Optionally start from POS."
   (interactive)
   (fedi--goto-pos #'previous-single-property-change))
 
-(defun fedi--property (prop)
-  "Get text property PROP from item at point."
-  (get-text-property (point) prop))
-
 (defun fedi-next-tab-item (&optional previous prop)
   "Move to the next interesting item.
 This could be the next toot, link, or image; whichever comes first.
 Don't move if nothing else to move to is found, i.e. near the end of the buffer.
 This also skips tab items in invisible text, i.e. hidden spoiler text.
-PREVIOUS means move to previous item."
+PREVIOUS means move to previous item.
+PROP is the text property to search for."
   (interactive)
   (let (next-range
         (search-pos (point)))
