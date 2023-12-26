47ba471009d0471a4125b3c863b71db2ae4de3a0
Thanos Apollo <public@thanosapollo.org>
2023-12-26 03:01:22 +0200

    Add test notes.el

---
+++ notes.el
@@ -0,0 +1,48 @@
+
+
+
+;; 
+(gnosis-add-deck "Biochemistry")
+(gnosis-add-note-mcq :deck "Biochemistry"
+		     :question "A type 1 diabetic is brought to the emergency department due to lethargy and rapid breathing. Blood measurements indicated elevated levels of glucose and ketone bodies. Blood pH was 7.1. The patient was exhibiting enhanced breathing to exhale which one of the following gases in order to correct the abnormal blood pH?"
+		     :choices '("Oxygen" "Nitrogen" "Nitrous Oxide"
+				"Carbon Dioxide" "Superoxide")
+		     :correct-answer 4
+		     :extra "The patient is in the midst of diabetic ketoacidosis, in which the production, but nonuse, of ketone bodies (which are acids) results in a significant lowering of blood pH. This patient will be creating a respiratory alkalosis to attempt to compensate for a metabolic acidosis. Under conditions of an acidosis, the proton concentration of the blood needs to be reduced. Due to the presence of carbonic anhy- drase in the red blood cell, as carbon dioxide is exhaled, protons are removed from solution. As the concentra- tion of carbon dioxide is reduced, bicarbonate (HCO3−) reacts with a proton (H+) to form carbonic acid, which then dissociates to form water (H2O) and carbon diox- ide (CO2). These reactions are summarized in the figure below. Thus, as carbon dioxide is exhaled, the proton concentration decreases, and the acidosis is reduced. The exhalation of oxygen or nitrogen will not affect the proton levels in the blood, nor will the loss of nitrous oxide or superoxide."
+		     :tags '("biochemistry" "amino-acids")
+		     :suspend t)
+;; 
+(gnosis-add-deck "Surgery")
+(gnosis-add-note-mcq :deck "Surgery"
+		     :question "A 35-year-old man is admitted with systolic blood pressure (BP) of 60 mm Hg and a heart rate (HR) of 150 bpm following a gunshot wound to the liver (Fig. 1–1). What is the effect on the kidneys?"
+		     :choices '("They tolerate satisfactorily ischemia of 3–4 hours duration."
+				"They undergo further ischemia if hypothermia is present."
+				"They can become damaged, even though urine output exceeds 1500 mL/d."
+				"They are affected and cause an increased creatinine clearance."
+				"They are prevented from further damage by a vasopressor.")
+		     :correct-answer 3
+		     :extra "High-output renal failure should be sus-
+pected if the BUN continues to rise with urine output >1000–1500 mL/d.
+It is associated with mild-to-moderate renal insufficiency; in
+comparison, severe renal injury results in olig- uric renal failure.
+The kidneys do not tolerate ischemia for more than 30–90 minutes.
+Hypothermia is protective. There is a decrease in creatinine
+clearance. Vasopressors aggravate the deleterious effects of shock."
+		     :image "2023.png"
+		     :tags "surgery renal kidneys")
+;; 
+(gnosis-add-note-mcq :deck "Anatomy"
+		     :question "A 37-year-old man is admitted to the
+emergency department after a severe car crash. After examining the
+patient the emergency medicine physician concludes that the serratus
+anterior muscle is damaged. Which of the following nerves innervates
+the serratus anterior muscle?"
+		     :choices '("Long thoracic" "Axillary" "Spinal accessory" "Dorsal scapular" "Thoracodorsal")
+		     :correct-answer 1
+		     :extra "The long thoracic is the only nerve that
+innervates the serratus anterior. The axillary nerve innervates the
+deltoid, the spinal accessory nerve innervates the sternocleidomastoid
+and trapezius, the dorsal scapular nerve supplies the rhomboid muscles
+and levator scapulae, and the latissimus dorsi is the muscle supplied
+by the thoracodorsal nerve."
+		     :tags "anatomy thoracic serratus-anterior")
