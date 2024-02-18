51d5b4661e59b6ac291aa887391689372a7e4843
Thanos Apollo <public@thanosapollo.org>
2024-02-18 16:37:20 +0200

    version bump: 0.1.7

--- gnosis.el
+++ gnosis.el
@@ -5,7 +5,7 @@
 ;; Author: Thanos Apollo <public@thanosapollo.org>
 ;; Keywords: extensions
 ;; URL: https://thanosapollo.org/projects/gnosis
-;; Version: 0.1.6
+;; Version: 0.1.7
 
 ;; Package-Requires: ((emacs "27.2") (compat "29.1.4.2") (emacsql "20240124"))
 
