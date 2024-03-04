a3ff4c5db800aa76231de5ae990deee92903939f
Thanos Apollo <public@thanosapollo.org>
2024-03-04 03:37:03 +0200

    packaging: Update package commentary

--- gnosis.el
+++ gnosis.el
@@ -24,14 +24,16 @@
 
 ;;; Commentary:
 
-;; Gnosis, pronounced "noh-sis", is a spaced repetition system for
-;; note taking & self testing, where notes are taken in a
-;; Question/Answer/Explanation format & reviewed in spaced
-;; intervals.
-;; 
-;; Gnosis can help you better understand and retain the material by
-;; encouraging active engagement.  It also provides a clear structure for
-;; your notes & review sessions, making it easier to study.
+;; Gnosis, is a spaced repetition system for note taking & self
+;; testing, where notes are taken in a Question/Answer/Explanation
+;; format & reviewed in spaced intervals, determined by the success or
+;; failure to recall a given answer for question.
+
+;; Gnosis implements a highly customizable algorithm, inspired by SM-2
+;; that is used by Anki.  Gnosis algorithm does not use user's
+;; subjective rating of a note to determine the next review interval,
+;; but instead uses the user's success or failure in recalling the
+;; answer of a note.
 
 ;;; Code:
 
