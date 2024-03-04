e7c1fa915c4aa1f2a8aba35293cfb65466bf9096
Hugh Daschbach <hugh@ccss.com>
2024-03-04 07:10:34 -0800

    Fix broken jabber-parse-time.
    Timezone computation broken by the expectation that `split-string'
    will preserve the SEPARATORS in the result.

--- lisp/jabber-util.el
+++ lisp/jabber-util.el
@@ -476,16 +476,10 @@ TIME is in a format accepted by `format-time-string'."
 	 (day (string-to-number (substring time 8 10)))
 	 (hour (string-to-number (substring time 11 13)))
 	 (minute (string-to-number (substring time 14 16)))
-	 (second (string-to-number (substring time 17 19)))
-         (timezone (if (eq (aref time 19) ?.)
-                       ;; fractions are optional
-                       (let ((timezone (cadr
-                                        (split-string (substring time 20)
-                                                      "[-+Z]"))))
-                         (if (string= "" timezone)
-                             "Z"
-                           timezone))
-                     (substring time 19))))
+         (tail (substring time 17))
+         (tz (string-match "[Z+-]" tail))
+	 (second (string-to-number (substring tail 0 tz)))
+         (timezone (if tz (substring tail tz) "Z")))
     ;; timezone is either Z (UTC) or [+-]HH:MM
     (let ((timezone-seconds
 	   (if (string= timezone "Z")
@@ -493,7 +487,15 @@ TIME is in a format accepted by `format-time-string'."
 	     (* (if (eq (aref timezone 0) ?+) 1 -1)
 		(* 60 (+ (* 60 (string-to-number (substring timezone 1 3)))
 			 (string-to-number (substring timezone 4 6))))))))
-      (encode-time (list second minute hour day month year nil -1 timezone-seconds)))))
+      ;; XEP-0082 states: The fractions of a second are optional and
+      ;; MAY be ignored if included (although an XMPP protocol
+      ;; extension using the DateTime profile MAY require the
+      ;; fractions of a second).
+      ;;
+      ;; Currently the display logic in ‘jabber-chat.chokes on
+      ;; fractional seconds.  Truncate ‘seconds’ to an integer until
+      ;; the display logic can be reworked.
+      (encode-time (list (floor second) minute hour day month year nil -1 timezone-seconds)))))
 
 (defun jabber-report-success (_jc xml-data context)
   "IQ callback reporting success or failure of the operation.
