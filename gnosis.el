f2fe739076e095e4c701593fc1978a6268a0d3a0
Thanos Apollo <public@thanosapollo.org>
2024-03-05 03:44:20 +0200

    package: Update commentary

--- gnosis.el
+++ gnosis.el
@@ -29,11 +29,10 @@
 ;; format & reviewed in spaced intervals, determined by the success or
 ;; failure to recall a given answer for question.
 
-;; Gnosis implements a highly customizable algorithm, inspired by SM-2
-;; that is used by Anki.  Gnosis algorithm does not use user's
-;; subjective rating of a note to determine the next review interval,
-;; but instead uses the user's success or failure in recalling the
-;; answer of a note.
+;; Gnosis implements a highly customizable algorithm, inspired by SM-2.
+;; Gnosis algorithm does not use user's subjective rating of a note to
+;; determine the next review interval, but instead uses the user's
+;; success or failure in recalling the answer of a note.
 
 ;;; Code:
 
