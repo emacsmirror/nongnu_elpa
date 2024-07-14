f3dceb8e679207db68c73d6114e5a10923fd390f
Thanos Apollo <public@thanosapollo.org>
2024-07-14 06:14:50 +0300

    demo: Update cloze contents

--- gnosis.el
+++ gnosis.el
@@ -5,7 +5,7 @@
 ;; Author: Thanos Apollo <public@thanosapollo.org>
 ;; Keywords: extensions
 ;; URL: https://thanosapollo.org/projects/gnosis
-;; Version: 0.3.0
+;; Version: 0.3.1
 
 ;; Package-Requires: ((emacs "27.2") (emacsql "20240124") (compat "29.1.4.2"))
 
@@ -2284,8 +2284,7 @@ If STRING-SECTION is nil, apply FACE to the entire STRING."
 				   :extra "Athens (Αθήνα) is the largest city of Greece & one of the world's oldest cities, with it's recorded history spanning over 3,500 years."
 				   :tags note-tags)
 	     (gnosis-add-note--cloze :deck deck-name
-				     :note "GNU Emacs is an extensible editor created by {c1:Richard} {c1:Stallman} in {c2:1984}"
-				     :hint ""
+				     :note "GNU Emacs is an extensible editor created by {{c1::Richard}} {{c1::Stallman}} in {{c2::1984::year}}"
 				     :tags note-tags
 				     :extra "Emacs was originally implemented in 1976 on the MIT AI Lab's Incompatible Timesharing System (ITS), as a collection of TECO macros. The name “Emacs” was originally chosen as an abbreviation of “Editor MACroS”. =This version of Emacs=, GNU Emacs, was originally *written in 1984*")
 	     (gnosis-add-note--y-or-n :deck deck-name
