4d9651981887d9de424ad0250f9be2894d813e3c
marty hiatt <martianhiatus@riseup.net>
2024-10-04 08:14:44 +0200

    add tp to Package-Requires

--- lisp/mastodon.el
+++ lisp/mastodon.el
@@ -7,7 +7,8 @@
 ;;         Marty Hiatt <martianhiatus@riseup.net>
 ;; Maintainer: Marty Hiatt <martianhiatus@riseup.net>
 ;; Version: 1.0.27
-;; Package-Requires: ((emacs "27.1") (request "0.3.0") (persist "0.4"))
+;; Package-Requires: ((emacs "27.1") (request "0.3.0")
+;;                    (persist "0.4") (tp "0.1"))
 ;; Homepage: https://codeberg.org/martianh/mastodon.el
 
 ;; This file is not part of GNU Emacs.
