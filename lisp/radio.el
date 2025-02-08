cd79d98e369874dbc966c5e1cbd2320e5f315a9d
Roi Martin <jroi.martin@gmail.com>
2025-02-08 09:36:37 +0100

    lisp/radio.el: update version

--- lisp/radio.el
+++ lisp/radio.el
@@ -5,7 +5,7 @@
 ;; Author: Roi Martin <jroi.martin@gmail.com>
 ;; Maintainer: Roi Martin <jroi.martin@gmail.com>
 ;; URL: https://github.com/jroimartin/radio
-;; Version: 0.1.0
+;; Version: 0.1.3
 ;; Package-Requires: ((emacs "29.1"))
 ;; Keywords: multimedia
 
