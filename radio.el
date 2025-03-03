9dc4f8bdfd32593df15f9de45a2a40934bb7f423
Roi Martin <jroi.martin@gmail.com>
2025-03-03 23:35:26 +0100

    radio.el: bump Version to 0.4.0

--- radio.el
+++ radio.el
@@ -5,7 +5,7 @@
 ;; Author: Roi Martin <jroi.martin@gmail.com>
 ;; Maintainer: Roi Martin <jroi.martin@gmail.com>
 ;; URL: https://github.com/jroimartin/radio
-;; Version: 0.3.1
+;; Version: 0.4.0
 ;; Package-Requires: ((emacs "29.1"))
 ;; Keywords: multimedia
 
