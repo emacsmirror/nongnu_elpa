535ad422905ca4b7eac2d98e28f149600be02821
marty hiatt <martianhiatus@disroot.org>
2025-03-16 12:50:03 +0100

    add quote region when commenting. FIX #95. (relies on fedi.el update)

--- fj.el
+++ fj.el
@@ -2887,32 +2887,35 @@ TYPE is a symbol of what we are composing, it may be issue or comment.
 Inject INIT-TEXT into the buffer, for editing."
   (interactive)
   (setq fj-compose-last-buffer (buffer-name (current-buffer)))
-  (fedi-post--compose-buffer
-   edit
-   #'markdown-mode
-   (or mode #'fj-compose-mode)
-   (when mode "fj-compose")
-   (or type 'issue)
-   (list #'fj-compose-mentions-capf
-         #'fj-compose-issues-capf)
-   ;; TODO: why not have a compose-buffer-spec rather than 10 separate vars?
-   `(((name . "repo")
-      (prop . compose-repo)
-      (item-var . fj-compose-repo)
-      (face . link))
-     ((name . ,(if (eq type 'comment) "issue ""title"))
-      (prop . compose-title)
-      (item-var . fj-compose-issue-title)
-      (face . fj-post-title-face)))
-   init-text)
-  (setq fj-compose-item-type
-        (if edit
+  (let ((quote (when (region-active-p)
+                 (buffer-substring (region-beginning)
+                                   (region-end)))))
+    (fedi-post--compose-buffer
+     edit
+     #'markdown-mode
+     (or mode #'fj-compose-mode)
+     (when mode "fj-compose")
+     (or type 'issue)
+     (list #'fj-compose-mentions-capf
+           #'fj-compose-issues-capf)
+     ;; TODO: why not have a compose-buffer-spec rather than 10 separate vars?
+     `(((name . "repo")
+        (prop . compose-repo)
+        (item-var . fj-compose-repo)
+        (face . link))
+       ((name . ,(if (eq type 'comment) "issue ""title"))
+        (prop . compose-title)
+        (item-var . fj-compose-issue-title)
+        (face . fj-post-title-face)))
+     init-text quote)
+    (setq fj-compose-item-type
+          (if edit
+              (if (eq type 'comment)
+                  'edit-comment
+                'edit-issue)
             (if (eq type 'comment)
-                'edit-comment
-              'edit-issue)
-          (if (eq type 'comment)
-              'new-comment
-            'new-issue))))
+                'new-comment
+              'new-issue)))))
 
 (defun fj-compose-send ()
   "Submit the issue or comment to your Forgejo instance.
