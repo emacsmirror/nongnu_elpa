d7bf6796fb51a7de314d9bc2680b3584373fd994
marty hiatt <martianhiatus@disroot.org>
2025-03-23 13:03:40 +0100

    add recent activities types list and data fetch. #117.

--- fj.el
+++ fj.el
@@ -3831,6 +3831,46 @@ Returns a list of strings."
        (lambda (_)
          (message "Tag %s deleted!" choice))))))
 
+;;; ACTIVITIES
+
+(defvar fj-activity-types
+  '(create_repo
+    rename_repo
+    star_repo
+    watch_repo
+    commit_repo
+    create_issue
+    create_pull_request
+    transfer_repo
+    push_tag
+    comment_issue
+    merge_pull_request
+    close_issue
+    reopen_issue
+    close_pull_request
+    reopen_pull_request
+    delete_tag
+    delete_branch
+    mirror_sync_push
+    mirror_sync_create
+    mirror_sync_delete
+    approve_pull_request
+    reject_pull_request
+    comment_pull
+    publish_release
+    pull_review_dismissed
+    pull_request_ready_for_review
+    auto_merge_pull_request))
+
+(defun fj-get-activities (&optional user-only)
+  "Return data for recent activities timeline for `fj-user'.
+If USER-ONLY, limit results to only those performed by `fj-user'."
+  ;; GET /users/{username}/activities/feeds
+  (let* ((endpoint (format "/users/%s/activities/feeds" fj-user)))
+    ;; items to display: .op_type, .act_user.login, repo.name
+    ;; .created .comment/issue.body, commit refs, etc.
+    (fj-get endpoint user-only)))
+
 ;;; INSPECT DATA
 
 (defun fj-inspect-item-data (&optional property)
