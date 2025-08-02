d4b8c8afbf01a03ccd49fcae326bb5ea99537f60
marty hiatt <martianhiatus@disroot.org>
2025-08-02 11:12:38 +0200

    bump

--- fj.el
+++ fj.el
@@ -6,7 +6,7 @@
 ;; Package-Requires: ((emacs "29.1") (fedi "0.2") (tp "0.5") (transient) (magit))
 ;; Keywords: git, convenience
 ;; URL: https://codeberg.org/martianh/fj.el
-;; Version: 0.19
+;; Version: 0.20
 ;; Separator: -
 
 ;; This program is free software; you can redistribute it and/or modify
