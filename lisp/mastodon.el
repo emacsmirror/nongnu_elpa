26109947d27e7f442eec1909ef744735d9f20854
marty hiatt <martianhiatus@disroot.org>
2025-10-06 19:37:49 +0200

    bump persist

--- lisp/mastodon.el
+++ lisp/mastodon.el
@@ -7,7 +7,7 @@
 ;;         Marty Hiatt <mousebot@disroot.org>
 ;; Maintainer: Marty Hiatt <mousebot@disroot.org>
 ;; Version: 2.0.2
-;; Package-Requires: ((emacs "28.1") (persist "0.4") (tp "0.7"))
+;; Package-Requires: ((emacs "28.1") (persist "0.7") (tp "0.7"))
 ;; Homepage: https://codeberg.org/martianh/mastodon.el
 
 ;; This file is not part of GNU Emacs.
