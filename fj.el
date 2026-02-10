6b126f1b2aeaf956793007d8351f2c458eaa936d
marty hiatt <martianhiatus@riseup.net>
2026-02-10 16:01:08 +0100

    add repo activity types var

--- fj.el
+++ fj.el
@@ -4977,9 +4977,39 @@ PAGE and LIMIT as always."
         'help-echo .sha)
        "\n" fedi-horiz-bar fedi-horiz-bar "\n\n"))))
 
+(defvar fj-repo-activity-types
+  '("create_repo"
+    "rename_repo"
+    "star_repo"
+    "watch_repo"
+    "commit_repo"
+    "create_issue"
+    "create_pull_request"
+    "transfer_repo"
+    "push_tag"
+    "comment_issue"
+    "merge_pull_request"
+    "close_issue"
+    "reopen_issue"
+    "close_pull_request"
+    "reopen_pull_request"
+    "delete_tag"
+    "delete_branch"
+    "mirror_sync_push"
+    "mirror_sync_create"
+    "mirror_sync_delete"
+    "approve_pull_request"
+    "reject_pull_request"
+    "comment_pull"
+    "publish_release"
+    "pull_review_dismissed"
+    "pull_request_ready_for_review"
+    "auto_merge_pull_request")
+  "List of activity types in repository feeds.")
+
 ;; GET /repos/{owner}/{repo}/activities/feeds
 (defun fj-repo-get-feed (repo owner)
-  "Get te activity feed of REPO by OWNER."
+  "Get the activity feed of REPO by OWNER."
   (let ((endpoint (format "repos/%s/%s/activities/feeds" owner repo)))
     (fj-get endpoint)))
 
