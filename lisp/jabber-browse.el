dbd5f34c186204332361c876de748e5cc88bd7c1
Thanos Apollo <public@thanosapollo.org>
2026-03-13 15:20:20 +0200

    [Refactor] Decompose jabber-process-browse into focused helpers.
    Extract category heading lookup into a data-driven alist and a
    jabber-browse--category-heading function, and move per-item rendering
    into jabber-browse--insert-item.  Add Commentary section; fix header
    to reference XEP-0011 instead of JEP-0011.

--- lisp/jabber-browse.el
+++ lisp/jabber-browse.el
@@ -1,4 +1,4 @@
-;;; jabber-browse.el --- jabber browsing by JEP-0011  -*- lexical-binding: t; -*-
+;;; jabber-browse.el --- jabber browsing by XEP-0011  -*- lexical-binding: t; -*-
 
 ;; Copyright (C) 2002, 2003, 2004 - tom berger - object@intelectronica.net
 ;; Copyright (C) 2003, 2004 - Magnus Henoch - mange@freemail.hu
@@ -19,15 +19,23 @@
 ;; along with this program; if not, write to the Free Software
 ;; Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
+;;; Commentary:
+
+;; Legacy entity browsing via jabber:iq:browse (XEP-0011, deprecated).
+;; Sends browse queries and renders results (users, services,
+;; conferences) into a buffer.  Read-only: we answer no browse
+;; requests.  Modern servers use Service Discovery (XEP-0030) instead;
+;; see `jabber-disco.el'.
+
+;;; Code:
+
 (require 'jabber-iq)
 (require 'jabber-xml)
 (require 'jabber-util)
 (require 'jabber-menu)
 
-;; jabber.el can perform browse requests, but will not answer them.
+(add-to-list 'jabber-jid-info-menu (cons "Send browse query" 'jabber-get-browse))
 
-(add-to-list 'jabber-jid-info-menu
-	     (cons "Send browse query" 'jabber-get-browse))
 (defun jabber-get-browse (jc to)
   "Send a browse infoquery request to someone.
 
@@ -40,7 +48,48 @@ JC is the Jabber connection."
                   #'jabber-process-data #'jabber-process-browse
 		  #'jabber-process-data "Browse failed"))
 
-;; called from jabber-process-data
+(defconst jabber-browse--category-labels
+  '((user       . "$ USER")
+    (service    . "* SERVICE")
+    (conference . "@ CONFERENCE"))
+  "Alist mapping browse category symbols to display labels.")
+
+(defun jabber-browse--category-heading (item)
+  "Return a heading string for browse ITEM."
+  (let* ((name (jabber-xml-node-name item))
+         (category (jabber-xml-get-attribute item 'category))
+         (label (or (cdr (assq name jabber-browse--category-labels))
+                    (cdr (assq (intern-soft (or category ""))
+                               jabber-browse--category-labels))
+                    (format "! OTHER: %s"
+                            (if (and category (> (length category) 0))
+                                category
+                              name)))))
+    (propertize label 'face 'jabber-title-medium)))
+
+(defun jabber-browse--insert-item (jc item)
+  "Insert a single browse result ITEM into the current buffer.
+JC is the Jabber connection."
+  (let ((jid (jabber-xml-get-attribute item 'jid))
+        (beginning (point)))
+    (insert (jabber-browse--category-heading item) "\n\n")
+    (dolist (attr '((type . "Type:\t\t")
+                    (jid . "JID:\t\t")
+                    (name . "Name:\t\t")
+                    (version . "Version:\t")))
+      (let ((data (jabber-xml-get-attribute item (car attr))))
+        (when (> (length data) 0)
+          (insert (cdr attr) data "\n"))))
+    (dolist (ns (jabber-xml-get-children item 'ns))
+      (let ((text (car (jabber-xml-node-children ns))))
+        (when (stringp text)
+          (insert "Namespace:\t" text "\n"))))
+    (insert "\n")
+    (put-text-property beginning (point) 'jabber-jid jid)
+    (put-text-property beginning (point) 'jabber-account jc)
+    (when (listp (car (jabber-xml-node-children item)))
+      (jabber-process-browse jc item))))
+
 (defun jabber-process-browse (jc xml-data)
   "Handle results from jabber:iq:browse requests.
 
@@ -49,58 +98,8 @@ XML-DATA is the parsed tree data from the stream (stanzas)
 obtained from `xml-parse-region'."
   (dolist (item (jabber-xml-node-children xml-data))
     (when (and (listp item)
-	       (not (eq (jabber-xml-node-name item) 'ns)))
-      (let ((jid (jabber-xml-get-attribute item 'jid))
-	    (beginning (point)))
-	(cond
-	 ((or
-	   (eq (jabber-xml-node-name item) 'user)
-	   (string= (jabber-xml-get-attribute item 'category) "user"))
-	  (insert (propertize "$ USER"
-			      'face 'jabber-title-medium)
-		  "\n\n"))
-	 ((or
-	   (eq (jabber-xml-node-name item) 'service)
-	   (string= (jabber-xml-get-attribute item 'category) "service"))
-	  (insert (propertize "* SERVICE"
-			      'face 'jabber-title-medium)
-		  "\n\n"))
-	 ((or
-	   (eq (jabber-xml-node-name item) 'conference)
-	   (string= (jabber-xml-get-attribute item 'category) "conference"))
-	  (insert (propertize "@ CONFERENCE"
-			      'face 'jabber-title-medium)
-		  "\n\n"))
-	 (t
-	  ;; So far I've seen "server" and "directory", both in the node-name.
-	  ;; Those are actually service disco categories, but jabberd 2 seems
-	  ;; to use them for browse results as well.  It's not right (as in
-	  ;; XEP-0011), but it's reasonable.
-	  (let ((category (jabber-xml-get-attribute item 'category)))
-	    (if (= (length category) 0)
-		(setq category (jabber-xml-node-name item)))
-	    (insert (propertize (format "! OTHER: %s" category)
-				'face 'jabber-title-medium)
-		    "\n\n"))))
-	(dolist (attr '((type . "Type:\t\t")
-			(jid . "JID:\t\t")
-			(name . "Name:\t\t")
-			(version . "Version:\t")))
-	  (let ((data (jabber-xml-get-attribute item (car attr))))
-	    (if (> (length data) 0)
-		(insert (cdr attr) data "\n"))))
-
-	(dolist (ns (jabber-xml-get-children item 'ns))
-	  (if (stringp (car (jabber-xml-node-children ns)))
-	      (insert "Namespace:\t" (car (jabber-xml-node-children ns)) "\n")))
-
-	(insert "\n")
-	(put-text-property beginning (point) 'jabber-jid jid)
-	(put-text-property beginning (point) 'jabber-account jc)
-
-	;; XXX: Is this kind of recursion really needed?
-	(if (listp (car (jabber-xml-node-children item)))
-	    (jabber-process-browse jc item))))))
+               (not (eq (jabber-xml-node-name item) 'ns)))
+      (jabber-browse--insert-item jc item))))
 
 (provide 'jabber-browse)
 ;;; jabber-browse.el ends here.
