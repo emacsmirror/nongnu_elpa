f58c49d526684d630065632f1fc4e16ab001e5f5
Thanos Apollo <public@thanosapollo.org>
2026-03-17 00:05:21 +0200

    [fix] use :eval for receipt header-line to preserve faces

--- lisp/jabber-chat.el
+++ lisp/jabber-chat.el
@@ -73,7 +73,7 @@ These fields are about your account:
     " " (:eval (jabber-fix-status (get (jabber-jid-symbol jabber-chatting-with) 'status)))
     " " jabber-chat-encryption-message	;see jabber-chatbuffer.el
     jabber-chatstates-message		;see jabber-chatstates.el
-    jabber-chat-receipt-message)		;see jabber-receipts.el
+    (:eval jabber-chat-receipt-message))	;see jabber-receipts.el
   "The specification for the header line of chat buffers.
 The format is that of `mode-line-format' and `header-line-format'.")
 
