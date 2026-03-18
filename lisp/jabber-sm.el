b7492d0b63092a526845da8b7295207c3af2104d
Thanos Apollo <public@thanosapollo.org>
2026-03-18 20:46:03 +0200

    add XEP-0198 Stream Management module
    Counter arithmetic with 2^32 wraparound, stanza counting,
    ack send/receive, queue management, enable/resume XML
    generation, periodic ack request timer.  34 ERT tests.

---
+++ lisp/jabber-sm.el
@@ -0,0 +1,298 @@
+;;; jabber-sm.el --- XEP-0198 Stream Management  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2026  Thanos Apollo
+
+;; Maintainer: Thanos Apollo <public@thanosapollo.org>
+
+;; This file is a part of jabber.el.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program; if not, write to the Free Software
+;; Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+;;; Commentary:
+
+;; XEP-0198 Stream Management provides two features:
+;;
+;; 1. Stanza acknowledgement: counters tracking what each side received,
+;;    so undelivered stanzas can be detected and retransmitted.
+;;
+;; 2. Stream resumption: fast reconnect that skips SASL auth and
+;;    preserves the server-side session.
+;;
+;; SM state is stored on the FSM state-data plist.  All functions in this
+;; file are pure (take state-data, return values) except for the timer
+;; management and the send helpers that call `jabber-send-string'.
+
+;;; Code:
+
+(require 'cl-lib)
+
+(declare-function jabber-send-string "jabber-core.el" (jc string))
+(declare-function jabber-xml-node-name "jabber-xml.el" (node))
+(declare-function jabber-xml-get-attribute "jabber-xml.el" (node attribute))
+(declare-function jabber-xml-get-xmlns "jabber-xml.el" (node))
+(declare-function jabber-xml-get-children "jabber-xml.el" (node child-name))
+(declare-function jabber-xml-child-with-xmlns "jabber-xml.el" (node xmlns))
+(declare-function jabber-sexp2xml "jabber-xml.el" (sexp))
+(defvar jabber-debug-log-xml)
+
+(defconst jabber-sm-xmlns "urn:xmpp:sm:3"
+  "XEP-0198 Stream Management namespace (version 3).")
+
+(defgroup jabber-sm nil
+  "XEP-0198 Stream Management."
+  :group 'jabber)
+
+(defcustom jabber-sm-enable t
+  "If non-nil, negotiate Stream Management when the server supports it."
+  :type 'boolean
+  :group 'jabber-sm)
+
+(defcustom jabber-sm-request-interval 30
+  "Seconds between periodic ack requests."
+  :type 'integer
+  :group 'jabber-sm)
+
+;;; Counter arithmetic (handles 2^32 wraparound per XEP-0198 section 5)
+
+(defconst jabber-sm--counter-max (expt 2 32)
+  "Stanza counters wrap at 2^32.")
+
+(defun jabber-sm--inc-counter (n)
+  "Increment counter N, wrapping at 2^32."
+  (mod (1+ n) jabber-sm--counter-max))
+
+(defun jabber-sm--counter-delta (a b)
+  "Return the forward distance from counter B to counter A.
+Both values are mod 2^32.  Result is in [0, 2^32)."
+  (mod (- a b) jabber-sm--counter-max))
+
+(defun jabber-sm--counter-<= (a b)
+  "Return non-nil if counter A is at or behind counter B.
+Uses forward-distance heuristic: if delta(B,A) < 2^31, A <= B."
+  (< (jabber-sm--counter-delta b a) (/ jabber-sm--counter-max 2)))
+
+;;; Predicates for SM XML elements
+
+(defun jabber-sm--r-p (stanza)
+  "Return non-nil if STANZA is an SM <r/> request."
+  (and (eq (jabber-xml-node-name stanza) 'r)
+       (equal (jabber-xml-get-xmlns stanza) jabber-sm-xmlns)))
+
+(defun jabber-sm--a-p (stanza)
+  "Return non-nil if STANZA is an SM <a/> acknowledgement."
+  (and (eq (jabber-xml-node-name stanza) 'a)
+       (equal (jabber-xml-get-xmlns stanza) jabber-sm-xmlns)))
+
+(defun jabber-sm--enabled-p (stanza)
+  "Return non-nil if STANZA is an SM <enabled/> response."
+  (and (eq (jabber-xml-node-name stanza) 'enabled)
+       (equal (jabber-xml-get-xmlns stanza) jabber-sm-xmlns)))
+
+(defun jabber-sm--resumed-p (stanza)
+  "Return non-nil if STANZA is an SM <resumed/> response."
+  (and (eq (jabber-xml-node-name stanza) 'resumed)
+       (equal (jabber-xml-get-xmlns stanza) jabber-sm-xmlns)))
+
+(defun jabber-sm--failed-p (stanza)
+  "Return non-nil if STANZA is an SM <failed/> response."
+  (and (eq (jabber-xml-node-name stanza) 'failed)
+       (equal (jabber-xml-get-xmlns stanza) jabber-sm-xmlns)))
+
+;;; State-data management
+
+(defconst jabber-sm--initial-keys
+  '(:sm-enabled nil
+    :sm-id nil
+    :sm-resume-max nil
+    :sm-outbound-count 0
+    :sm-inbound-count 0
+    :sm-outbound-queue nil
+    :sm-last-acked 0
+    :sm-resuming nil
+    :sm-resumed nil
+    :sm-r-timer nil)
+  "Initial SM keys for the FSM state-data plist.")
+
+(defun jabber-sm--reset (state-data)
+  "Return STATE-DATA with all SM keys reset to initial values."
+  (let ((keys jabber-sm--initial-keys))
+    (while keys
+      (setq state-data (plist-put state-data (car keys) (cadr keys)))
+      (setq keys (cddr keys))))
+  state-data)
+
+(defun jabber-sm--init (state-data)
+  "Return STATE-DATA with SM keys initialized (same as reset)."
+  (jabber-sm--reset state-data))
+
+;;; Stream features check
+
+(defun jabber-sm--features-have-sm-p (state-data)
+  "Return non-nil if stream features in STATE-DATA include SM."
+  (let ((features (plist-get state-data :stream-features)))
+    (when features
+      (jabber-xml-child-with-xmlns features jabber-sm-xmlns))))
+
+;;; Stanza counting
+
+(defun jabber-sm--stanza-p (sexp)
+  "Return non-nil if SEXP is a countable stanza (message, presence, or iq)."
+  (memq (jabber-xml-node-name sexp) '(message presence iq)))
+
+(defun jabber-sm--count-outbound (state-data sexp)
+  "Increment outbound counter and queue SEXP if SM is enabled.
+Return updated STATE-DATA."
+  (when (and (plist-get state-data :sm-enabled)
+             (jabber-sm--stanza-p sexp))
+    (let ((count (jabber-sm--inc-counter
+                  (plist-get state-data :sm-outbound-count))))
+      (setq state-data (plist-put state-data :sm-outbound-count count))
+      (setq state-data
+            (plist-put state-data :sm-outbound-queue
+                       (append (plist-get state-data :sm-outbound-queue)
+                               (list (cons count sexp)))))))
+  state-data)
+
+(defun jabber-sm--count-inbound (state-data stanza)
+  "Increment inbound counter if SM is enabled and STANZA is countable.
+Return updated STATE-DATA."
+  (when (and (plist-get state-data :sm-enabled)
+             (jabber-sm--stanza-p stanza))
+    (setq state-data
+          (plist-put state-data :sm-inbound-count
+                     (jabber-sm--inc-counter
+                      (plist-get state-data :sm-inbound-count)))))
+  state-data)
+
+;;; Ack send/receive
+
+(defun jabber-sm--make-ack-xml (h)
+  "Return the XML string for <a h='H' xmlns='urn:xmpp:sm:3'/>."
+  (format "<a xmlns='%s' h='%d'/>" jabber-sm-xmlns h))
+
+(defun jabber-sm--make-request-xml ()
+  "Return the XML string for <r xmlns='urn:xmpp:sm:3'/>."
+  (format "<r xmlns='%s'/>" jabber-sm-xmlns))
+
+(defun jabber-sm--send-ack (jc state-data)
+  "Send an <a/> ack to JC with inbound count from STATE-DATA."
+  (jabber-send-string jc (jabber-sm--make-ack-xml
+                          (plist-get state-data :sm-inbound-count))))
+
+(defun jabber-sm--request-ack (jc)
+  "Send an <r/> request to JC."
+  (jabber-send-string jc (jabber-sm--make-request-xml)))
+
+(defun jabber-sm--prune-queue (queue h)
+  "Return QUEUE with entries whose count is <= H removed."
+  (cl-remove-if (lambda (entry)
+                  (jabber-sm--counter-<= (car entry) h))
+                queue))
+
+(defun jabber-sm--process-ack (state-data stanza)
+  "Process an incoming <a/> ack STANZA, pruning the outbound queue.
+Return updated STATE-DATA."
+  (let* ((h (string-to-number (or (jabber-xml-get-attribute stanza 'h) "0")))
+         (queue (plist-get state-data :sm-outbound-queue))
+         (pruned (jabber-sm--prune-queue queue h)))
+    (setq state-data (plist-put state-data :sm-last-acked h))
+    (setq state-data (plist-put state-data :sm-outbound-queue pruned))
+    state-data))
+
+;;; Enable/resume XML generation
+
+(defun jabber-sm--make-enable-xml ()
+  "Return the XML string for <enable resume='true' xmlns='urn:xmpp:sm:3'/>."
+  (format "<enable xmlns='%s' resume='true'/>" jabber-sm-xmlns))
+
+(defun jabber-sm--make-resume-xml (h previd)
+  "Return the XML string for <resume h='H' previd='PREVID'/>."
+  (format "<resume xmlns='%s' h='%d' previd='%s'/>" jabber-sm-xmlns h previd))
+
+(defun jabber-sm--parse-enabled (stanza)
+  "Parse an <enabled/> STANZA.
+Return a plist (:id ID :resume RESUME :max MAX)."
+  (list :id (jabber-xml-get-attribute stanza 'id)
+        :resume (equal (jabber-xml-get-attribute stanza 'resume) "true")
+        :max (let ((max-str (jabber-xml-get-attribute stanza 'max)))
+               (when max-str (string-to-number max-str)))))
+
+(defun jabber-sm--apply-enabled (state-data enabled-info)
+  "Apply parsed ENABLED-INFO to STATE-DATA, enabling SM.
+Return updated STATE-DATA."
+  (setq state-data (plist-put state-data :sm-enabled t))
+  (setq state-data (plist-put state-data :sm-id (plist-get enabled-info :id)))
+  (when (plist-get enabled-info :max)
+    (setq state-data (plist-put state-data :sm-resume-max
+                                (plist-get enabled-info :max))))
+  state-data)
+
+;;; Resume handling
+
+(defun jabber-sm--handle-resumed (state-data stanza)
+  "Process <resumed/> STANZA against STATE-DATA after stream resumption.
+Prune the outbound queue per the server's h value.
+Return (UPDATED-STATE-DATA . STANZAS-TO-RESEND)."
+  (let* ((h (string-to-number (or (jabber-xml-get-attribute stanza 'h) "0")))
+         (queue (plist-get state-data :sm-outbound-queue))
+         (pruned (jabber-sm--prune-queue queue h))
+         (to-resend (mapcar #'cdr pruned)))
+    (setq state-data (plist-put state-data :sm-last-acked h))
+    (setq state-data (plist-put state-data :sm-outbound-queue nil))
+    (setq state-data (plist-put state-data :sm-resumed t))
+    (setq state-data (plist-put state-data :sm-resuming nil))
+    (cons state-data to-resend)))
+
+;;; Periodic ack request timer
+
+(defun jabber-sm--start-r-timer (jc state-data)
+  "Start a periodic <r/> timer for connection JC.
+Return updated STATE-DATA with the timer stored."
+  (jabber-sm--stop-r-timer state-data)
+  (let ((timer (run-with-timer jabber-sm-request-interval
+                               jabber-sm-request-interval
+                               #'jabber-sm--request-ack jc)))
+    (plist-put state-data :sm-r-timer timer)))
+
+(defun jabber-sm--stop-r-timer (state-data)
+  "Cancel the periodic <r/> timer if running.
+Return updated STATE-DATA."
+  (let ((timer (plist-get state-data :sm-r-timer)))
+    (when (timerp timer)
+      (cancel-timer timer)))
+  (plist-put state-data :sm-r-timer nil))
+
+;;; FSM routing helper
+
+(defun jabber-sm--maybe-enable-or-establish (state-data)
+  "Return FSM transition for STATE-DATA to :sm-enable or :session-established.
+Checks `jabber-sm-enable' and whether stream features include SM."
+  (if (and jabber-sm-enable
+           (jabber-sm--features-have-sm-p state-data))
+      (list :sm-enable state-data)
+    (list :session-established state-data)))
+
+;;; Post-connect hook entry point
+
+(defun jabber-sm-maybe-start (jc)
+  "Start SM ack timer if SM was successfully enabled on JC.
+Intended for `jabber-post-connect-hooks'."
+  (let ((state-data (fsm-get-state-data jc)))
+    (when (plist-get state-data :sm-enabled)
+      (jabber-sm--start-r-timer jc state-data))))
+
+(provide 'jabber-sm)
+
+;;; jabber-sm.el ends here
---
+++ tests/jabber-sm-tests.el
@@ -0,0 +1,279 @@
+;;; jabber-sm-tests.el --- Tests for jabber-sm  -*- lexical-binding: t; -*-
+
+(require 'ert)
+(require 'jabber-xml)
+(require 'jabber-sm)
+
+;;; Counter arithmetic
+
+(ert-deftest jabber-sm-test-inc-counter ()
+  "Incrementing a counter adds one."
+  (should (= (jabber-sm--inc-counter 0) 1))
+  (should (= (jabber-sm--inc-counter 41) 42)))
+
+(ert-deftest jabber-sm-test-inc-counter-wraps ()
+  "Counter wraps at 2^32."
+  (should (= (jabber-sm--inc-counter (1- (expt 2 32))) 0)))
+
+(ert-deftest jabber-sm-test-counter-delta ()
+  "Forward distance between counters."
+  (should (= (jabber-sm--counter-delta 5 3) 2))
+  (should (= (jabber-sm--counter-delta 0 (1- (expt 2 32))) 1)))
+
+(ert-deftest jabber-sm-test-counter-<= ()
+  "Counter comparison with wraparound."
+  (should (jabber-sm--counter-<= 3 5))
+  (should (jabber-sm--counter-<= 3 3))
+  (should-not (jabber-sm--counter-<= 5 3))
+  ;; Wraparound: counter near max is "before" counter near 0
+  (should (jabber-sm--counter-<= (- (expt 2 32) 2) 1)))
+
+;;; Predicates
+
+(ert-deftest jabber-sm-test-r-p ()
+  "Detect SM <r/> element."
+  (should (jabber-sm--r-p '(r ((xmlns . "urn:xmpp:sm:3")))))
+  (should-not (jabber-sm--r-p '(r ((xmlns . "wrong")))))
+  (should-not (jabber-sm--r-p '(a ((xmlns . "urn:xmpp:sm:3"))))))
+
+(ert-deftest jabber-sm-test-a-p ()
+  "Detect SM <a/> element."
+  (should (jabber-sm--a-p '(a ((xmlns . "urn:xmpp:sm:3") (h . "5")))))
+  (should-not (jabber-sm--a-p '(r ((xmlns . "urn:xmpp:sm:3"))))))
+
+(ert-deftest jabber-sm-test-enabled-p ()
+  "Detect SM <enabled/> element."
+  (should (jabber-sm--enabled-p
+           '(enabled ((xmlns . "urn:xmpp:sm:3") (id . "abc") (resume . "true"))))))
+
+(ert-deftest jabber-sm-test-resumed-p ()
+  "Detect SM <resumed/> element."
+  (should (jabber-sm--resumed-p
+           '(resumed ((xmlns . "urn:xmpp:sm:3") (h . "5") (previd . "abc"))))))
+
+(ert-deftest jabber-sm-test-failed-p ()
+  "Detect SM <failed/> element."
+  (should (jabber-sm--failed-p
+           '(failed ((xmlns . "urn:xmpp:sm:3"))))))
+
+;;; State-data reset
+
+(ert-deftest jabber-sm-test-reset ()
+  "Reset clears all SM keys to defaults."
+  (let* ((sd (list :username "test" :sm-enabled t :sm-outbound-count 42))
+         (result (jabber-sm--reset sd)))
+    (should-not (plist-get result :sm-enabled))
+    (should (= (plist-get result :sm-outbound-count) 0))
+    (should (= (plist-get result :sm-inbound-count) 0))
+    (should (null (plist-get result :sm-outbound-queue)))
+    ;; Non-SM keys preserved
+    (should (equal (plist-get result :username) "test"))))
+
+(ert-deftest jabber-sm-test-init ()
+  "Init is identical to reset."
+  (let ((sd (list :username "test")))
+    (should (= (plist-get (jabber-sm--init sd) :sm-outbound-count) 0))))
+
+;;; Features check
+
+(ert-deftest jabber-sm-test-features-have-sm ()
+  "Detect SM in stream features."
+  (let ((sd (list :stream-features
+                  '(features ((xmlns . "http://etherx.jabber.org/streams"))
+                             (bind ((xmlns . "urn:ietf:params:xml:ns:xmpp-bind")))
+                             (sm ((xmlns . "urn:xmpp:sm:3")))))))
+    (should (jabber-sm--features-have-sm-p sd))))
+
+(ert-deftest jabber-sm-test-features-no-sm ()
+  "No SM in stream features."
+  (let ((sd (list :stream-features
+                  '(features ((xmlns . "http://etherx.jabber.org/streams"))
+                             (bind ((xmlns . "urn:ietf:params:xml:ns:xmpp-bind")))))))
+    (should-not (jabber-sm--features-have-sm-p sd))))
+
+(ert-deftest jabber-sm-test-features-nil ()
+  "Nil stream features."
+  (should-not (jabber-sm--features-have-sm-p '(:stream-features nil))))
+
+;;; Stanza counting
+
+(ert-deftest jabber-sm-test-count-outbound-message ()
+  "Outbound message increments counter and queues."
+  (let* ((sd (list :sm-enabled t :sm-outbound-count 0 :sm-outbound-queue nil))
+         (msg '(message ((to . "bob@example.com")) (body () "hi")))
+         (result (jabber-sm--count-outbound sd msg)))
+    (should (= (plist-get result :sm-outbound-count) 1))
+    (should (= (length (plist-get result :sm-outbound-queue)) 1))
+    (should (equal (cdar (plist-get result :sm-outbound-queue)) msg))))
+
+(ert-deftest jabber-sm-test-count-outbound-iq ()
+  "Outbound iq increments counter."
+  (let* ((sd (list :sm-enabled t :sm-outbound-count 5 :sm-outbound-queue nil))
+         (iq '(iq ((type . "get") (id . "1")) (query ((xmlns . "test")))))
+         (result (jabber-sm--count-outbound sd iq)))
+    (should (= (plist-get result :sm-outbound-count) 6))))
+
+(ert-deftest jabber-sm-test-count-outbound-disabled ()
+  "No counting when SM is disabled."
+  (let* ((sd (list :sm-enabled nil :sm-outbound-count 0 :sm-outbound-queue nil))
+         (msg '(message ((to . "bob@example.com")) (body () "hi")))
+         (result (jabber-sm--count-outbound sd msg)))
+    (should (= (plist-get result :sm-outbound-count) 0))
+    (should (null (plist-get result :sm-outbound-queue)))))
+
+(ert-deftest jabber-sm-test-count-outbound-non-stanza ()
+  "SM elements are not counted."
+  (let* ((sd (list :sm-enabled t :sm-outbound-count 0 :sm-outbound-queue nil))
+         (r '(r ((xmlns . "urn:xmpp:sm:3"))))
+         (result (jabber-sm--count-outbound sd r)))
+    (should (= (plist-get result :sm-outbound-count) 0))))
+
+(ert-deftest jabber-sm-test-count-inbound ()
+  "Inbound stanza increments counter."
+  (let* ((sd (list :sm-enabled t :sm-inbound-count 0))
+         (msg '(message ((from . "bob@example.com")) (body () "hi")))
+         (result (jabber-sm--count-inbound sd msg)))
+    (should (= (plist-get result :sm-inbound-count) 1))))
+
+(ert-deftest jabber-sm-test-count-inbound-disabled ()
+  "No counting when SM is disabled."
+  (let* ((sd (list :sm-enabled nil :sm-inbound-count 0))
+         (msg '(message ((from . "bob@example.com")) (body () "hi")))
+         (result (jabber-sm--count-inbound sd msg)))
+    (should (= (plist-get result :sm-inbound-count) 0))))
+
+;;; Queue pruning and ack processing
+
+(ert-deftest jabber-sm-test-prune-queue ()
+  "Prune removes entries with count <= h."
+  (let* ((queue (list (cons 1 'a) (cons 2 'b) (cons 3 'c) (cons 4 'd)))
+         (result (jabber-sm--prune-queue queue 2)))
+    (should (= (length result) 2))
+    (should (= (caar result) 3))))
+
+(ert-deftest jabber-sm-test-prune-queue-empty ()
+  "Prune on empty queue returns empty."
+  (should (null (jabber-sm--prune-queue nil 5))))
+
+(ert-deftest jabber-sm-test-process-ack ()
+  "Processing <a/> prunes queue and updates last-acked."
+  (let* ((sd (list :sm-enabled t
+                   :sm-outbound-queue (list (cons 1 'a) (cons 2 'b) (cons 3 'c))
+                   :sm-last-acked 0))
+         (ack '(a ((xmlns . "urn:xmpp:sm:3") (h . "2"))))
+         (result (jabber-sm--process-ack sd ack)))
+    (should (= (plist-get result :sm-last-acked) 2))
+    (should (= (length (plist-get result :sm-outbound-queue)) 1))
+    (should (= (caar (plist-get result :sm-outbound-queue)) 3))))
+
+;;; FSM routing helper
+
+(ert-deftest jabber-sm-test-maybe-enable-with-sm ()
+  "Route to :sm-enable when SM is available."
+  (let ((jabber-sm-enable t)
+        (sd (list :stream-features
+                  '(features ((xmlns . "http://etherx.jabber.org/streams"))
+                             (sm ((xmlns . "urn:xmpp:sm:3")))))))
+    (should (eq (car (jabber-sm--maybe-enable-or-establish sd))
+                :sm-enable))))
+
+(ert-deftest jabber-sm-test-maybe-enable-without-sm ()
+  "Route to :session-established when SM is not in features."
+  (let ((jabber-sm-enable t)
+        (sd (list :stream-features
+                  '(features ((xmlns . "http://etherx.jabber.org/streams"))))))
+    (should (eq (car (jabber-sm--maybe-enable-or-establish sd))
+                :session-established))))
+
+(ert-deftest jabber-sm-test-maybe-enable-disabled ()
+  "Route to :session-established when SM is disabled by user."
+  (let ((jabber-sm-enable nil)
+        (sd (list :stream-features
+                  '(features ((xmlns . "http://etherx.jabber.org/streams"))
+                             (sm ((xmlns . "urn:xmpp:sm:3")))))))
+    (should (eq (car (jabber-sm--maybe-enable-or-establish sd))
+                :session-established))))
+
+;;; Enable/resume XML generation
+
+(ert-deftest jabber-sm-test-make-enable-xml ()
+  "Enable XML matches expected format."
+  (should (string-match-p "enable" (jabber-sm--make-enable-xml)))
+  (should (string-match-p "resume='true'" (jabber-sm--make-enable-xml))))
+
+(ert-deftest jabber-sm-test-make-resume-xml ()
+  "Resume XML includes h and previd."
+  (let ((xml (jabber-sm--make-resume-xml 42 "session-123")))
+    (should (string-match-p "h='42'" xml))
+    (should (string-match-p "previd='session-123'" xml))))
+
+(ert-deftest jabber-sm-test-parse-enabled ()
+  "Parse <enabled/> stanza."
+  (let ((info (jabber-sm--parse-enabled
+               '(enabled ((xmlns . "urn:xmpp:sm:3")
+                          (id . "abc-123")
+                          (resume . "true")
+                          (max . "300"))))))
+    (should (equal (plist-get info :id) "abc-123"))
+    (should (eq (plist-get info :resume) t))
+    (should (= (plist-get info :max) 300))))
+
+(ert-deftest jabber-sm-test-apply-enabled ()
+  "Apply enabled info to state-data."
+  (let* ((sd (list :sm-enabled nil :sm-id nil :sm-resume-max nil))
+         (info (list :id "abc" :resume t :max 300))
+         (result (jabber-sm--apply-enabled sd info)))
+    (should (eq (plist-get result :sm-enabled) t))
+    (should (equal (plist-get result :sm-id) "abc"))
+    (should (= (plist-get result :sm-resume-max) 300))))
+
+;;; Resume handling
+
+(ert-deftest jabber-sm-test-handle-resumed ()
+  "Handle <resumed/> prunes queue and returns stanzas to resend."
+  (let* ((msg-a '(message ((to . "a@x")) (body () "a")))
+         (msg-b '(message ((to . "b@x")) (body () "b")))
+         (msg-c '(message ((to . "c@x")) (body () "c")))
+         (sd (list :sm-enabled t
+                   :sm-outbound-queue (list (cons 1 msg-a)
+                                            (cons 2 msg-b)
+                                            (cons 3 msg-c))
+                   :sm-last-acked 0
+                   :sm-resumed nil
+                   :sm-resuming t))
+         (resumed '(resumed ((xmlns . "urn:xmpp:sm:3") (h . "1") (previd . "abc"))))
+         (result (jabber-sm--handle-resumed sd resumed)))
+    ;; state-data updated
+    (should (= (plist-get (car result) :sm-last-acked) 1))
+    (should (null (plist-get (car result) :sm-outbound-queue)))
+    (should (eq (plist-get (car result) :sm-resumed) t))
+    (should-not (plist-get (car result) :sm-resuming))
+    ;; stanzas to resend: entries 2 and 3
+    (should (= (length (cdr result)) 2))
+    (should (equal (car (cdr result)) msg-b))
+    (should (equal (cadr (cdr result)) msg-c))))
+
+(ert-deftest jabber-sm-test-handle-resumed-all-acked ()
+  "All stanzas acked means nothing to resend."
+  (let* ((sd (list :sm-enabled t
+                   :sm-outbound-queue (list (cons 1 'a) (cons 2 'b))
+                   :sm-last-acked 0
+                   :sm-resumed nil
+                   :sm-resuming t))
+         (resumed '(resumed ((xmlns . "urn:xmpp:sm:3") (h . "5") (previd . "abc"))))
+         (result (jabber-sm--handle-resumed sd resumed)))
+    (should (null (cdr result)))))
+
+;;; Ack XML generation
+
+(ert-deftest jabber-sm-test-make-ack-xml ()
+  "Ack XML includes h value."
+  (should (string-match-p "h='7'" (jabber-sm--make-ack-xml 7))))
+
+(ert-deftest jabber-sm-test-make-request-xml ()
+  "Request XML is well-formed."
+  (should (string-match-p "<r xmlns=" (jabber-sm--make-request-xml))))
+
+(provide 'jabber-sm-tests)
+
+;;; jabber-sm-tests.el ends here
