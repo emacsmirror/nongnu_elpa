b563a12042094931e4067a33497c3985dafa2227
Thanos Apollo <public@thanosapollo.org>
2026-03-31 07:29:19 +0300

    Rewrite jabber-info.el with targeted buffer routing
    Add jabber-info--make-callback that creates a closure routing
    all IQ results to a specific browse buffer. Query functions now
    use jabber-send-iq directly with the targeted callback instead
    of delegating to jabber-get-version etc. which hardcoded
    jabber-process-data as the callback.
    
    This fixes the bug where per-resource responses (version, disco,
    ping, time) created separate browse buffers instead of landing in
    the bare JID buffer.

--- lisp/jabber-info.el
+++ lisp/jabber-info.el
@@ -25,32 +25,122 @@
 
 ;; Global reference declarations
 
-(declare-function jabber-get-version "jabber-version.el" (jc to))
-(declare-function jabber-get-disco-info "jabber-disco.el" (jc to &optional node))
 (declare-function jabber-ping-send "jabber-ping.el"
                   (jc to process-func on-success on-error))
 (declare-function jabber-browse--buffer "jabber-iq.el" (jid))
-(declare-function jabber-muc-read-nickname "jabber-muc.el" (group prompt))
+(declare-function jabber-browse--insert "jabber-iq.el"
+                  (jc xml-data closure-data))
+(declare-function jabber-send-iq "jabber-iq.el"
+                  (jc to type query success-callback success-closure-data
+                      error-callback error-closure-data &optional result-id))
+(declare-function jabber-muc-read-nickname "jabber-muc.el"
+                  (group prompt))
+(declare-function jabber-process-version "jabber-version.el"
+                  (jc xml-data))
+(declare-function jabber-process-disco-info "jabber-disco.el"
+                  (jc xml-data))
+(declare-function jabber-process-ping "jabber-ping.el"
+                  (jc xml-data))
+(declare-function jabber-process-time "jabber-time.el"
+                  (jc xml-data))
+(declare-function jabber-process-last "jabber-time.el"
+                  (jc xml-data))
 
 (defvar jabber-group)                   ; jabber-muc.el
 (defvar jabber-buffer-connection)        ; jabber-chatbuffer.el
 
+(defun jabber-info--connected-resources (bare-jid)
+  "Return list of full JIDs for connected resources of BARE-JID."
+  (let* ((sym (jabber-jid-symbol bare-jid))
+         (resources (get sym 'resources))
+         (full-jids nil))
+    (dolist (entry resources)
+      (when (plist-get (cdr entry) 'connected)
+        (push (concat bare-jid "/" (car entry)) full-jids)))
+    (nreverse full-jids)))
+
+(defun jabber-info--make-callback (target-jid)
+  "Return a callback that routes results to TARGET-JID's browse buffer."
+  (lambda (jc xml-data closure-data)
+    (with-current-buffer (jabber-browse--buffer target-jid)
+      (jabber-browse--insert jc xml-data closure-data))))
+
+(defun jabber-info--query-bare (jc bare-jid)
+  "Fire queries appropriate for a bare JID.
+Results are routed to BARE-JID's browse buffer."
+  (let ((cb (jabber-info--make-callback bare-jid)))
+    (jabber-send-iq jc bare-jid "get"
+                    '(query ((xmlns . "http://jabber.org/protocol/disco#info")))
+                    cb #'jabber-process-disco-info
+                    cb "Disco info request failed")
+    (jabber-send-iq jc bare-jid "get"
+                    '(query ((xmlns . "jabber:iq:last")))
+                    cb #'jabber-process-last
+                    cb "Last online request failed")))
+
+(defun jabber-info--query-resource (jc full-jid target-jid)
+  "Fire queries appropriate for FULL-JID.
+Results are routed to TARGET-JID's browse buffer."
+  (let ((cb (jabber-info--make-callback target-jid)))
+    (jabber-send-iq jc full-jid "get"
+                    '(query ((xmlns . "jabber:iq:version")))
+                    cb #'jabber-process-version
+                    cb "Version request failed")
+    (jabber-send-iq jc full-jid "get"
+                    '(query ((xmlns . "http://jabber.org/protocol/disco#info")))
+                    cb #'jabber-process-disco-info
+                    cb "Disco info request failed")
+    (jabber-ping-send jc full-jid
+                      cb #'jabber-process-ping "Ping is unsupported")
+    (jabber-send-iq jc full-jid "get"
+                    '(time ((xmlns . "urn:xmpp:time")))
+                    cb #'jabber-process-time
+                    cb "Time request failed")
+    (jabber-send-iq jc full-jid "get"
+                    '(query ((xmlns . "jabber:iq:last")))
+                    cb #'jabber-process-last
+                    cb "Idle time request failed")))
+
 ;;;###autoload
 (defun jabber-get-info (jc to)
-  "Query JC for version, disco info and ping of TO in parallel.
+  "Query JC for all available info about TO.
+For a bare JID, queries disco info, last activity, and also
+queries each connected resource for version, disco, ping, time
+and idle time.  For a full JID, queries the resource directly.
 Results appear in the browse buffer for TO."
   (interactive (list (jabber-read-account)
                      (jabber-read-jid-completing
                       "Get info for: " nil nil nil 'full t)))
-  (with-current-buffer (jabber-browse--buffer to)
-    (let ((inhibit-read-only t))
-      (erase-buffer)
-      (insert (propertize to 'face 'jabber-title) "\n\n")))
-  (jabber-get-version jc to)
-  (jabber-get-disco-info jc to)
-  (jabber-ping-send jc to
-                    #'jabber-process-data #'jabber-process-ping
-                    "Ping is unsupported"))
+  (let* ((bare (jabber-jid-user to))
+         (resource (jabber-jid-resource to))
+         (full-jids (if resource
+                        (list to)
+                      (jabber-info--connected-resources bare))))
+    (let ((buf (jabber-browse--buffer bare)))
+      (with-current-buffer buf
+        (let ((inhibit-read-only t))
+          (erase-buffer)
+          (insert (propertize bare 'face 'jabber-title) "\n\n")))
+      ;; Bare JID queries (account-level info)
+      (unless resource
+        (jabber-info--query-bare jc bare))
+      ;; Per-resource queries
+      (if full-jids
+          (dolist (full-jid full-jids)
+            (with-current-buffer buf
+              (let ((inhibit-read-only t))
+                (goto-char (point-max))
+                (insert (propertize (concat "Resource: "
+                                           (jabber-jid-resource full-jid))
+                                   'face 'jabber-title)
+                        "\n\n")))
+            (jabber-info--query-resource jc full-jid bare))
+        (unless resource
+          (with-current-buffer buf
+            (let ((inhibit-read-only t))
+              (goto-char (point-max))
+              (insert "No connected resources found.\n\n")))))
+      (display-buffer buf))))
 
 ;;;###autoload
 (defun jabber-muc-get-info (jc group nickname)
@@ -60,7 +150,14 @@ JC is the Jabber connection."
   (interactive
    (jabber-muc-argument-list
     (list (jabber-muc-read-nickname jabber-group "Nickname: "))))
-  (jabber-get-info jc (format "%s/%s" group nickname)))
+  (let* ((full-jid (format "%s/%s" group nickname))
+         (buf (jabber-browse--buffer full-jid)))
+    (with-current-buffer buf
+      (let ((inhibit-read-only t))
+        (erase-buffer)
+        (insert (propertize full-jid 'face 'jabber-title) "\n\n")))
+    (jabber-info--query-resource jc full-jid full-jid)
+    (display-buffer buf)))
 
 (provide 'jabber-info)
 ;;; jabber-info.el ends here
