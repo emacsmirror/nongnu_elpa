dominikh/go-mode.el

go-mode.el: fix "unused variable num" waning
Fixes a:

    go-mode.el:2475:60: Warning: Unused lexical variable ‘num’

go-mode.el
@@ -2472,7 +2472,7 @@ divisor for FILE-NAME."
                (rest (split-string (nth 1 parts) "[., ]")))
 
           (cl-destructuring-bind
-              (start-line start-column end-line end-column num count)
+              (start-line start-column end-line end-column _ count)
               (mapcar #'string-to-number rest)
 
             (when (string= (file-name-nondirectory file) file-name)

emacsmirror/nongnu_elpa

go-mode.el
@@ -2472,7 +2472,7 @@ divisor for FILE-NAME."
                (rest (split-string (nth 1 parts) "[., ]")))
 
           (cl-destructuring-bind
-              (start-line start-column end-line end-column num count)
+              (start-line start-column end-line end-column _num count)
               (mapcar #'string-to-number rest)
 
             (when (string= (file-name-nondirectory file) file-name)