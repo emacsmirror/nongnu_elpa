84e57b17520edbadbdca17e0b2dba50c56134d74
Dmitry Gutov <dmitry@gutov.dev>
2026-06-04 00:19:53 +0300

    Bump version

--- flamegraph.el
+++ flamegraph.el
@@ -5,7 +5,7 @@
 ;; Author: Dmitry Gutov <dmitry@gutov.dev>
 ;; Homepage: https://github.com/dgutov/emacs-flamegraph/
 ;; Keywords: lisp, tools
-;; Version: 0.1
+;; Version: 0.2
 ;; Package-Requires: ((emacs "30.1"))
 
 ;; This file is free software: you can redistribute it and/or modify
