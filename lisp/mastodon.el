c4f82bd9750bb20708e8b8ab9ae335f245ebf0d9
marty hiatt <martianhiatus@riseup.net>
2026-08-08 14:05:03 +0200

    bump 2.1.0

--- lisp/mastodon.el
+++ lisp/mastodon.el
@@ -6,7 +6,7 @@
 ;; Author: Johnson Denen <johnson.denen@gmail.com>
 ;;         Marty Hiatt <martianh@disroot.org>
 ;; Maintainer: Marty Hiatt <martianh@disroot.org>
-;; Version: 2.0.17
+;; Version: 2.1.0
 ;; Package-Requires: ((emacs "29.1") (persist "0.8") (tp "0.8"))
 ;; Homepage: https://codeberg.org/martianh/mastodon.el
 
