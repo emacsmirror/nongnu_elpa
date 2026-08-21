737c1d6d2649b1ff82c5e2d2d9891a7caeac3835
Daniel Mendler <mail@daniel-mendler.de>
2026-08-21 14:05:51 +0200

    Version 4.2.0

--- elfeed.el
+++ elfeed.el
@@ -5,7 +5,7 @@
 ;; Author: Christopher Wellons <wellons@nullprogram.com>
 ;; Maintainer: Karthik Chikmagalur <karthik.chikmagalur@gmail.com>, Ihor Radchenko <yantar92@posteo.net>, Daniel Mendler <mail@daniel-mendler.de>
 ;; URL: https://github.com/emacs-elfeed/elfeed
-;; Version: 4.1.1
+;; Version: 4.2.0
 ;; Package-Requires: ((emacs "29.1") (compat "31"))
 ;; Keywords: network, comm, hypermedia
 
