b182612f6bd070af2d4e104ff36da0752b5e456f
Swithin Chan <swithinchan@yahoo.com.hk>
2026-08-23 08:32:28 +0800

    Add Assisted-by header to sapling.el

--- sapling.el
+++ sapling.el
@@ -3,6 +3,7 @@
 ;; Copyright (C) 2026  Free Software Foundation, Inc.
 
 ;; Author: Swithin Chan <swithinchan@yahoo.com.hk>
+;; Assisted-by: Deepseek:deepseek-v4-pro default
 ;; Version: 0.2.2
 ;; Package-Requires: ((emacs "26.1"))
 ;; Keywords: tools, vc
