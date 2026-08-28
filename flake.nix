a282f7be5707ee29d7e291a05eb1be8d7126ae9f
Thanos Apollo <public@thanosapollo.org>
2026-08-29 00:41:43 +0300

    Version bump: 0.4.0

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
         let
           pkgs = import nixpkgs { inherit system; };
           lib = pkgs.lib;
-          version = "0.3.3";
+          version = "0.4.0";
 
           emacs = pkgs.emacs30-nox or pkgs.emacs;
           emacsPackages = pkgs.emacsPackagesFor emacs;
