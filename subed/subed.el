d9317d1de190601384528ae01468688107725c8d
Sacha Chua <sacha@sachachua.com>
2026-09-17 22:51:40 -0400

    Bump version to 1.5.4 to see if that fixes version control

--- subed/subed.el
+++ subed/subed.el
@@ -1,6 +1,6 @@
 ;;; subed.el --- A major mode for editing subtitles  -*- lexical-binding: t; -*-
 
-;; Version: 1.5.3
+;; Version: 1.5.4
 ;; Maintainer: Sacha Chua <sacha@sachachua.com>
 ;; Author: Random User
 ;; Keywords: convenience, files, hypermedia, multimedia
